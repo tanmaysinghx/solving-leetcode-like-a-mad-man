fcaf4549e73965524f4a3581251fe303706aa87c
Tanmay Singh <tanmaysinghx@gmail.com>
2026-07-29 21:54:23 +0530

    Time: 13 ms (27.77%), Space: 46.7 MB (9.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for (int count : map.values()) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
