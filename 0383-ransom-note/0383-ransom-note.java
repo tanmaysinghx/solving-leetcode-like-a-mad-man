57ab8e855c371b9d7a62dfe488c49a4b0f272eee
Tanmay Singh <tanmaysinghx@gmail.com>
2026-07-29 21:53:05 +0530

    Time: 3 ms (50.91%), Space: 46.5 MB (57.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        if (magazine.length() < ransomNote.length()) return false;
+
         int[] count = new int[26];
 
-        for(char c : magazine.toCharArray()) {
-            count[c - 'a']++;
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
         }
 
-        for(char c : ransomNote.toCharArray()) {
-            count[c - 'a']--;
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+            count[index]--;
 
-            if (count[c - 'a'] < 0) {
+            if (count[index] < 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
